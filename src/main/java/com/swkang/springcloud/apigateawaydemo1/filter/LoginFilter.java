40a5722bc6aa18371ed4e402c17e407f4bc9492b
kangswx <996334837@qq.com>
2019-04-18 11:43:02 +0800

    代码优化

--- src/main/java/com/swkang/springcloud/apigateawaydemo1/filter/LoginFilter.java
+++ src/main/java/com/swkang/springcloud/apigateawaydemo1/filter/LoginFilter.java
@@ -41,20 +41,28 @@ public class LoginFilter extends ZuulFilter {
      */
     @Override
     public boolean shouldFilter() {
+
         RequestContext currentContext = RequestContext.getCurrentContext();
         HttpServletRequest request = currentContext.getRequest();
         String requestURI = request.getRequestURI();
         StringBuffer requestURL = request.getRequestURL();
         System.out.println("requestURI: " + requestURI);  ///apigateaway/order/api/v1/order/save
         System.out.println("requestURL: " + requestURL);  //http://localhost:9000/apigateaway/order/api/v1/order/save
-        if("/apigateaway/order/api/v1/order/save".equalsIgnoreCase(requestURI)){
+
+        if("/apigateaway/order/api/v1/order/save".equalsIgnoreCase(requestURI)){  //过滤特定的访问
             return true;
         }
         return false;
     }
 
+    /**
+     * shouldFilter为true，进行相关的业务校验
+     * @return
+     * @throws ZuulException
+     */
     @Override
     public Object run() throws ZuulException {
+
         System.out.println("启用拦截器");
 
         RequestContext currentContext = RequestContext.getCurrentContext();
@@ -66,10 +74,11 @@ public class LoginFilter extends ZuulFilter {
 
         //登录校验逻辑，可根据情况自定义JWT
         if(StringUtils.isBlank(token)){
-            currentContext.setSendZuulResponse(false);
-            currentContext.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+            currentContext.setSendZuulResponse(false);  //不允许访问
+            currentContext.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);  //设置返回码
         }
-        return null;
+        return null;  //允许访问
+
     }
 
 }
